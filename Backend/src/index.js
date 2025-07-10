b89b180bb42d6354b7e71558ac5fe484fdd2f786
BhawarSuthar7023 <bhawarsuthar7023@gmail.com>
2025-07-10 19:47:23 +0530

    cors error fix

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,7 +8,11 @@ const { default: cloudinaryUpload } = require("./utils/cloudinaryUpload")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*', // Allows all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
